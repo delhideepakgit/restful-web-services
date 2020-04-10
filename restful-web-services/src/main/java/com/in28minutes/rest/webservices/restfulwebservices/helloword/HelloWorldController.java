200c7963275811bfc4dc387b9ed873907726cf53
abc <abc@MININT-AEVSPO0>
2020-04-11 01:59:33 +0530

    Internationalization added

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloword;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/hello-world")
 	public String helloWorld()
@@ -26,4 +34,12 @@ public class HelloWorldController {
 	{
 		return new HelloWorldBean(String.format("Hello World %s", name));
 	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String internationalized()
+	{
+		return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
+	}
+	
+	
 }
